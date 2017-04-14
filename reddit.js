ce4e80c1d59c30dffe2446c20cd304d034b1d7a5
Halotitee <moreseed01@gmail.com>
2017-04-14 23:41:06 +0000

    backup reddit-crawl

--- reddit.js
+++ reddit.js
@@ -1,3 +1,4 @@
+'use strict'
 var bcrypt = require('bcrypt-as-promised');
 var HASH_ROUNDS = 10;
 
@@ -5,6 +6,7 @@ class RedditAPI {
     constructor(conn) {
         this.conn = conn;
     }
+
 
     createUser(user) {
         /*
@@ -31,19 +33,63 @@ class RedditAPI {
             });
     }
 
-    createPost(post) {
+    createSubreddit(subreddit) {
         return this.conn.query(
             `
-            INSERT INTO posts (userId, title, url, createdAt, updatedAt)
-            VALUES (?, ?, ?, NOW(). NOW())`,
-            [post.userId, post.title, post.url]
-        )
+            INSERT INTO subreddits (name, description, createdAt, updatedAt)
+            VALUES (?, ?, NOW(), NOW())`, [subreddit.name, subreddit.description]
+            ) 
             .then(result => {
                 return result.insertId;
+            })
+            .catch(error => {
+                
+                if(error.code === 'ER_DUP_ENTRY') {
+                    throw new Error('A subreddit with this name already exists! Try another topic :)')
+                } 
+                else {
+                    throw error;
+                }
             });
     }
-
-    getAllPosts(callback) {
+    
+    
+    createPost(post) {
+        return this.conn.query(
+            `INSERT INTO posts (userId, title, url, createdAt, updatedAt, subredditId) VALUES (?, ?, ?, NOW(), NOW(), ?)`,
+            [post.userId, post.title, post.url, post.subredditId])
+            .then(result => {
+                //console.log(result.subredditId);
+                return result.subredditId;
+            })
+            .catch(error => {
+                // Special error handling for no subreddit ID
+                if (error.code === 'ER_NO_SRID') {
+                    throw new Error('This Subreddit does not exist');
+                }
+                else {
+                    throw error;
+                }
+                });
+    }
+    
+    
+    createVote(vote) {
+            if(vote.voteDirection === 1 || vote.voteDirection === 0 || vote.voteDirection === -1) {
+                return this.conn.query(
+                    `
+                    INSERT INTO votes SET postId = ?, userId = ?, voteDirection = ? 
+                    ON DUPLICATE KEY UPDATE voteDirection = ?
+                    `
+                    [vote.postId, vote.userId, vote.voteDirection, vote.voteDirection])
+                    .catch(console.log);
+            } 
+            else {
+                throw new Error('Bad Vote');
+            }
+        }
+    
+    getAllPosts() {
         /*
         strings delimited with ` are an ES2015 feature called "template strings".
         they are more powerful than what we are using them for here. one feature of
@@ -55,11 +101,78 @@ class RedditAPI {
          */
         return this.conn.query(
             `
-            SELECT id, title, url, userId, createdAt, updatedAt
-            FROM posts
-            ORDER BY createdAt DESC
+            SELECT 
+                SUM(votes.voteDirection) AS votes_Score, 
+                posts.id AS posts_id, 
+                posts.title As posts_title, 
+                posts.url AS posts_url, 
+                posts.userId AS posts_userID, 
+                posts.createdAt AS posts_createdAt, 
+                posts.updatedAt AS posts_updatedAt,
+                users.id AS users_id,
+                users.username AS users_username,
+                users.createdAt AS users_createdAt, 
+                users.updatedAt AS users_updatedAt,
+                subreddits.id AS subreddits_id,
+                subreddits.name AS subreddits_name,
+                subreddits.description AS subreddits_description,
+                subreddits.url AS subreddits_url,
+                subreddits.createdAt AS subreddits_createdAt,
+                subreddits.updatedAt AS subreddits_updatedAt
+            FROM posts 
+            JOIN users ON users.id = posts.userId
+            JOIN subreddits ON posts.subredditId = subreddits.id
+            LEFT JOIN votes ON posts.id = votes.postId
+            GROUP BY votes.postId
+            ORDER BY votes_Score
             LIMIT 25`
-        );
+            
+            
+        ).then (function(result) {
+            return result.map(function(val) {
+                console.log(val);
+                    return {
+                        
+                        votes_Score: val.votes_Score,
+                        posts_id: val.posts_id,
+                        posts_title: val.posts_title,
+                        posts_url: val.posts_url,
+                        posts_createdAt: val.posts_createdAt,
+                        posts_updatedAt: val.posts_updatedAt,
+                        
+                        user:{
+                            users_id: val.users_id,
+                            users_username: val.users_username,
+                            users_createdAt: val.users_createdAt,
+                            users_updatedAt: val.users_updatedAt
+                        },
+                        
+                        subreddits:{
+                            subreddits_id: val.subreddits_id,
+                            subreddits_name: val.subreddits_name,
+                            subreddits_description: val.subreddits_description,
+                            subreddits_url: val.subreddits_url,
+                            subreddits_createdAt : val.subreddits_createdAt,
+                            subreddits_updatedAt: val.subreddits_updatedAt
+                        }
+                        
+                    }   
+                    //console.log('this is the result', val);
+            });
+        
+        });
+    }
+    
+    
+    getAllSubreddits() {
+        return this.conn.query(
+            `
+            SELECT *
+            FROM subreddits
+            ORDER BY subreddits.createdAt DESC 
+            LIMIT 25
+            `
+            );
     }
 }
 
--- reddit.sql
+++ reddit.sql
@@ -21,3 +21,32 @@ CREATE TABLE posts (
   KEY userId (userId), -- why did we add this here? ask me :)
   CONSTRAINT validUser FOREIGN KEY (userId) REFERENCES users (id) ON DELETE SET NULL
 );
+
+CREATE TABLE subreddits (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) DEFAULT NULL UNIQUE,
+  description VARCHAR(200),
+  url VARCHAR(2000) DEFAULT NULL,
+  userId INT DEFAULT NULL,
+  createdAt DATETIME NOT NULL,
+  updatedAt DATETIME NOT NULL
+);
+
+CREATE INDEX subredditName on subreddits(name);
+
+ALTER TABLE posts ADD COLUMN subredditId INT AFTER id;
+
+ALTER TABLE posts ADD CONSTRAINT subredditId
+  FOREIGN KEY (subredditId) REFERENCES subreddits (id);
+  
+CREATE TABLE votes (
+  userId INT,
+  postId INT,
+  voteDirection TINYINT,
+  createdAt DATETIME NOT NULL,
+  updatedAt DATETIME NOT NULL,
+  PRIMARY KEY (userId, postId), 
+  KEY userId (userId),
+  KEY postId (postId),
+  CONSTRAINT validVoteUser FOREIGN KEY (userId) REFERENCES users (id) ON DELETE CASCADE, 
+  CONSTRAINT validVotePost FOREIGN KEY (postId) REFERENCES posts (id) ON DELETE CASCADE);
